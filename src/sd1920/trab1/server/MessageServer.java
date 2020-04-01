338865bdaafdc50f0ec801ae89441acdb6d0213d
Dimas <p.dimas@campus.fct.unl.pt>
2020-04-01 02:41:15 +0100

    added Discovery to MessageServer

--- src/sd1920/trab1/server/MessageServer.java
+++ src/sd1920/trab1/server/MessageServer.java
@@ -4,15 +4,18 @@ import java.net.InetAddress;
 import java.net.URI;
 import java.net.UnknownHostException;
 import java.util.logging.Logger;
+import java.net.InetSocketAddress;
 
 import org.glassfish.jersey.jdkhttp.JdkHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 
+import sd1920.trab1.discovery.Discovery;
 import sd1920.trab1.server.resources.MessageResource;
 
 public class MessageServer {
 
 	private static Logger Log = Logger.getLogger(MessageServer.class.getName());
+	private static Discovery discovery_channel;
 
 	static {
 		System.setProperty("java.net.preferIPv4Stack", "true");
@@ -21,8 +24,12 @@ public class MessageServer {
 	
 	public static final int PORT = 8080;
 	public static final String SERVICE = "MessageService";
+
 	
 	public static void main(String[] args) throws UnknownHostException {
+
+		discovery_channel = new Discovery(new InetSocketAddress("226.226.226.226",2266),InetAddress.getLocalHost().getCanonicalHostName(),"http://"+InetAddress.getLocalHost().getHostAddress()+"/");
+
 		String ip = InetAddress.getLocalHost().getHostAddress();
 			
 		ResourceConfig config = new ResourceConfig();
