b6515bbbaae876d433fd2f3477bc5f711bd51be0
Dimas <p.dimas@campus.fct.unl.pt>
2020-04-02 17:14:18 +0100

    404 be gone thot

--- src/sd1920/trab1/server/resources/MessageResource.java
+++ src/sd1920/trab1/server/resources/MessageResource.java
@@ -204,7 +204,7 @@ public class MessageResource implements MessageService {
 		config.property(ClientProperties.READ_TIMEOUT, GetMessageClient.REPLY_TIMEOUT);
 
 		Client client = ClientBuilder.newClient(config);
-		WebTarget target = client.target("http://"+url+":8080").path(UserService.PATH).path(name).queryParam("pwd",pwd);
+		WebTarget target = client.target("http://"+url+":8080/rest").path(UserService.PATH).path(name).queryParam("pwd",pwd);
 
 		System.out.println("PATH + " + target);
 
